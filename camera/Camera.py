5893874a4d94055e7f42e2948cf7bf7c62942af3
Hammad Ali <hammad.a22002@gmail.com>
2023-12-17 14:48:34 +0500

    camera.py __main__ function

--- camera/Camera.py
+++ camera/Camera.py
@@ -1,12 +1,16 @@
 import cv2
+import argparse
+import time
 
 
 class Camera:
     def __init__(self, ip, port, username, password) -> None:
-        self.connection_str = f'rtsp://{username}:{password}@{ip}:{port}'
+        self.__init__(f'rtsp://{username}:{password}@{ip}:{port}')
 
     def __init__(self, connection_str):
         self.connection_str = connection_str
+        self.suspend_stream = False
+        self.suspend_time = 4
 
     def connect(self):
         self.cap = cv2.VideoCapture(self.connection_str)
@@ -14,13 +18,25 @@ class Camera:
             raise Exception("Video source not found")
 
     def disconnect(self):
+        cv2.destroyAllWindows()
         self.cap.release()
 
-    def get_frame(self):
+    def get_frame(self, show=False):
         ret, frame = self.cap.read()
         if not ret or frame is None:
-            self.disconnect()
+            if not self.suspend_stream:
+                print("Waiting for stream to resume")
+                self.suspend_stream = True
+                self.disconnect()
+                time.sleep(self.suspend_time)
+                self.connect()
+                return
             raise CameraDisconnected("Camera disconnected")
+        if show:
+            cv2.imshow("frame", frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                self.disconnect()
+                raise CameraDisconnected("Camera disconnected")
         return frame
 
     def get_fps(self):
@@ -29,3 +45,17 @@ class Camera:
 
 class CameraDisconnected(Exception):
     pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", type=str, required=True)
+    args = parser.parse_args()
+    camera = Camera(args.url)
+    camera.connect()
+    while True:
+        try:
+            frame = camera.get_frame(show=True)
+        except CameraDisconnected:
+            print("Stream ended")
+            break
