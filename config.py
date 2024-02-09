caacf1f53af962171394e6d39cf63e5f1fbc9ceb
Ibrahim <mibrahim37612@gmail.com>
2024-02-09 15:48:39 +0500

    Change server

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 DEVICE_ID = "abc"
-SERVER_URL = "http://localhost:5000"
-# SERVER_URL = "http://13.51.86.179:5500"
+# SERVER_URL = "http://localhost:5000"
+SERVER_URL = "http://13.51.86.179:5500"
 BASE_URL = f"{SERVER_URL}/api/"
 
 # Config for camera scanning
